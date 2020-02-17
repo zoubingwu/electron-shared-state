e7145b6f19df5b30fe18e5379d2bf5353ce71e1b
shadeofgod <zoubingwu@gmail.com>
2020-02-18 06:52:06 +0800

    rewrite example

--- example/renderer.js
+++ example/renderer.js
@@ -1,21 +1,18 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const { ipcRenderer } = require("electron");
 const { sharedState } = require("./shared");
 const { createSharedStore } = require("../dist/index");
 
 const store = createSharedStore(sharedState);
 const container = document.querySelector("#count");
-const btn = document.querySelector("#btn");
 store.subscribe((state, changeDescription) => {
   console.log("sharedState in renderer changed to: ", state, changeDescription);
   container.innerHTML = state.count;
 });
-btn.addEventListener("click", () => {
+document.querySelector("#inc").addEventListener("click", () => {
   store.setState(state => {
     state.count = state.count + 1;
   }, "+1 by renderer");
+});
+document.querySelector("#dec").addEventListener("click", () => {
+  ipcRenderer.send("decrement");
 });
